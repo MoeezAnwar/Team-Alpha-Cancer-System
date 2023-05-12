154737796f31fbe7791e3cb980f434c134eff4ba
MoeezAnwar <116732848+MoeezAnwar@users.noreply.github.com>
2023-05-13 02:21:10 +0500

    init3

--- Alpha/vite.config.js
+++ Alpha/vite.config.js
@@ -5,11 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+// vite.config.js
+
+export default {
+  base: '/',
+}
+
